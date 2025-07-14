9537dcb890c20dddd6045742d7008ccb067c65ba
fcamilalima <fcamilalima@gmail.com>
2025-07-14 03:22:31 -0300

    Criando menu com Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,14 +6,30 @@ import { CategoriaDetalhe } from './categoria-detalhe/categoria-detalhe';
 import { CategoriaEditar } from './categoria-editar/categoria-editar';
 import { Login } from './login/login';
 import { Logout } from './logout/logout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MenuComponent } from './menu/menu.component';
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CategoriaNova, CategoriaDetalhe, 
-    CategoriaEditar, Categorias, Login, Logout, RouterLink, RouterLinkActive],
+    CategoriaEditar, Categorias, Login, Logout, RouterLink, 
+    RouterLinkActive, MatButtonModule, MatCardModule, MatIconModule,
+    MatInputModule, MatListModule, MatProgressSpinnerModule, MatSelectModule,
+    MatSidenavModule, MatTableModule, MatToolbarModule, MenuComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected title = 'categorias';
 }
+
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrls: ['./login.scss']
 })
 export class Login {
 
